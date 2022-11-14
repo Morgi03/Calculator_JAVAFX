a9c9217f4c2ddef6a54ea1f2e75d26d2c63f1c19
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-14 14:01:39 +0100

    calculator-view elkészítése

--- src/main/java/hu/petrik/calculatorproject/calculator/CalculatorController.java
+++ src/main/java/hu/petrik/calculatorproject/calculator/CalculatorController.java
@@ -3,7 +3,7 @@ package hu.petrik.calculatorproject.calculator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CalculatorController {
     @FXML
     private Label welcomeText;
 
